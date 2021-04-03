578950e709e6dbd879f608bd54b9ffa13831e318
GetScoreSetScore <andy844551@gmail.com>
2021-04-04 02:46:31 +0300

    Add project files.

---
+++ HTTP_FileStorage_Client/Client.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Xml.Linq;
+using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Reflection.Metadata.Ecma335;
+using System.IO;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+
+namespace HTTP_FileStorage_Client
+{
+    class Client
+    {
+        static string ServerUrl = "http://127.0.0.1:8080";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ip сервера:");
+            string address = Console.ReadLine();
+            Console.WriteLine("Введите порт сервера:");
+            string port = Console.ReadLine();
+            ServerUrl = "http://"+address+":"+port+"/";
+            while (true)
+            {
+                string request = Console.ReadLine();
+                string[] parameters= request.Split();
+                if (parameters.Length > 0)
+                {
+                    switch (parameters[0])
+                    {
+                        case "GET":
+                            if (parameters.Length == 3)
+                            {
+                                GET(parameters[1], parameters[2]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is GET /serverfolder/file.ext /clientfolder");
+                            break;
+                        case "PUT":
+                            if (parameters.Length == 3)
+                            {
+                                PUT(parameters[1], parameters[2]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is PUT /serverfolder /clientfolder/file.ext");
+                            break;
+                        case "POST":
+                            if (parameters.Length == 3)
+                            {
+                                POST(parameters[1], parameters[2]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is POST /serverfolder /clientfolder/file.ext");
+                            break;
+                        case "COPY":
+                            if (parameters.Length == 3)
+                            {
+                                COPY(parameters[1], parameters[2]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is COPY /serverfolderfrom/file.ext /serverfolderto");
+                            break;
+                        case "MOVE":
+                            if (parameters.Length == 3)
+                            {
+                                MOVE(parameters[1], parameters[2]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is MOVE /serverfolderfrom/file.ext /serverfolderto");
+                            break;
+                        case "DELETE":
+                            if (parameters.Length == 2)
+                            {
+                                DELETE(parameters[1]);
+                            }
+                            else Console.WriteLine("Invalide parameters,syntax is DELETE /serverfolder/file.ext");
+                            break;
+                        default:
+                            Console.WriteLine("Invalide command, supported commands are GET,PUT,POST,COPY,MOVE,DELETE");
+                            break;
+                    }
+                }
+            }
+        }
+        public static HttpResponseMessage Request(string Method, string path, Byte[] content, Dictionary<string,string> headers)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                var client = new HttpClient();
+                var httpRequestMessage = new HttpRequestMessage
+                {
+                    Method = new HttpMethod(Method),
+                    RequestUri = new Uri(ServerUrl + path),
+                    Content = new ByteArrayContent(content)
+                };
+                foreach(KeyValuePair<string, string> entry in headers){
+                    httpRequestMessage.Headers.Add(entry.Key, entry.Value);
+                }
+                response = client.SendAsync(httpRequestMessage).Result;
+            }
+            catch
+            {
+                Console.WriteLine("Error sending request!");
+            }
+            return response;
+        }
+
+        public static void GET(string PathOnServer, string PathOnClient)
+        {
+            HttpResponseMessage response = Request("GET", PathOnServer, new byte[0], new Dictionary<string, string>());
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File GET successful");
+                Byte[] file;
+                try
+                {
+                    file = response.Content.ReadAsByteArrayAsync().Result;
+                    File.WriteAllBytes(Path.GetFullPath(PathOnClient.Replace("/", "\\")) +"//"+Path.GetFileName(PathOnServer), file);
+                }
+                catch
+                {
+                    Console.WriteLine("Error writing file!");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("GET unsuccessful, error: "+(response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+
+        public static void PUT(string PathOnServer, string PathOnClient)
+        {
+            Byte[] file;
+            try
+            {
+                file = File.ReadAllBytes(PathOnClient);
+            }
+            catch
+            {
+                Console.WriteLine("File not found!");
+                return;
+            }
+            Dictionary<string, string> headers = new Dictionary<string, string>{
+                { "FileName", Path.GetFileName(PathOnClient) }
+            };
+            HttpResponseMessage response = Request("PUT", PathOnServer, file,headers);
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File PUT successful");
+            }
+            else
+            {
+                Console.WriteLine("PUT unsuccessful, error: " + (response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+
+        public static void POST(string PathOnServer, string PathOnClient)
+        {
+            Byte[] file;
+            try
+            {
+                file = File.ReadAllBytes(PathOnClient);
+            }
+            catch
+            {
+                Console.WriteLine("File not found!");
+                return;
+            }
+            Dictionary<string, string> headers = new Dictionary<string, string>{
+                { "FileName", Path.GetFileName(PathOnClient) }
+            };
+            HttpResponseMessage response = Request("POST", PathOnServer, file,headers);
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File POST successful");
+            }
+            else
+            {
+                Console.WriteLine("POST unsuccessful, error: " + (response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+
+        public static void COPY(string PathFrom, string PathTo)
+        {
+
+            Dictionary<string, string> headers = new Dictionary<string, string>{
+                { "Pathto", Path.GetFileName(PathTo) }
+            };
+            HttpResponseMessage response = Request("COPY", PathFrom, new byte[0], headers);
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File COPY successful");
+            }
+            else
+            {
+                Console.WriteLine("COPY unsuccessful, error: " + (response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+
+        public static void MOVE(string PathFrom, string PathTo)
+        {
+            Dictionary<string, string> headers = new Dictionary<string, string>{
+                { "Pathto", Path.GetFileName(PathTo) }
+            };
+            HttpResponseMessage response = Request("MOVE", PathFrom, new byte[0],headers);
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File MOVE successful");
+            }
+            else
+            {
+                Console.WriteLine("MOVE unsuccessful, error: " + (response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+
+        public static void DELETE(string PathOnServer)
+        {
+            HttpResponseMessage response = Request("DELETE", PathOnServer, new byte[0],new Dictionary<string, string>());
+            if (response?.IsSuccessStatusCode ?? false)
+            {
+                Console.WriteLine("File DELETE successful");
+            }
+            else
+            {
+                Console.WriteLine("DELETE unsuccessful, error: " + (response?.StatusCode.ToString() ?? "response from server was not received"));
+            }
+        }
+    }
+}
---
+++ HTTP_FileStorage_Server/Server.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net.Http;
+using System.Net;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Net.NetworkInformation;
+namespace HTTP_FileStorage_Server
+{
+    class Server
+    {
+        public static bool PortInUse(int port)
+        {
+            bool inUse = false;
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+            foreach (IPEndPoint endPoint in ipEndPoints)
+            {
+                if (endPoint.Port == port)
+                {
+                    inUse = true;
+                    break;
+                }
+            }
+            return inUse;
+        }
+        static string StorageFolder = @"D:\University\2course\4semester\labs\KSiS\laba3\HTTP_FileStorage\Storage";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Available ip addresses:");
+            List<string> adresses = new List<string>();
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus != OperationalStatus.Up) continue;
+                {
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address.ToString());
+                            adresses.Add(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Select preferable ip");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (!adresses.Contains(message))
+                {
+                    Console.WriteLine("Invalid ip, repeat input");
+                }
+                else
+                {
+                    ip = message;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Select preferable port");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (PortInUse(Int32.Parse(message)))
+                {
+                    Console.WriteLine("Port in use");
+                }
+                else if (Int32.Parse(message) < 1024)
+                {
+                    Console.WriteLine("Select port greater than 1024");
+                }
+                else
+                {
+                    port = message;
+                    break;
+                }
+            }
+            Console.WriteLine("Select folder or Enter for default");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message == "")
+                {
+                    break;
+                }
+                else if (Directory.Exists(message))
+                {
+                    StorageFolder = message;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Folder not found");
+                }
+            }
+            Thread thread = new Thread(StartListening);
+            thread.Start();
+        }
+        static string port;
+        static string ip;
+        private static void StartListening()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://"+ip+":"+port+"/");
+            listener.Start();
+            Console.WriteLine("Listening...");
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                HttpListenerResponse response = context.Response;
+                switch (request.HttpMethod)
+                {
+                    case "GET":
+                        PerformGET(request, response);
+                        break;
+                    case "PUT":
+                        PerformPUT(request, response);
+                        break;
+                    case "POST":
+                        PerformPOST(request, response);
+                        break;
+                    case "COPY":
+                        PerformCOPY(request, response);
+                        break;
+                    case "MOVE":
+                        PerformMOVE(request, response);
+                        break;
+                    case "DELETE":
+                        PerformDELETE(request, response);
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.NotImplemented;
+                        response.OutputStream.Close();
+                        break;
+                }
+            }
+        }
+        static void PerformGET(HttpListenerRequest request,HttpListenerResponse response) {
+            try
+            {
+                if (File.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+                {
+                    byte[] file = File.ReadAllBytes(StorageFolder + (request.RawUrl).Replace("/", "\\"));
+                    response.ContentLength64 = file.Length;
+                    response.SendChunked = false;
+                    response.OutputStream.Write(file, 0, file.Length);
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+        static void PerformPUT(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                if (Directory.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+                {
+
+                    byte[] file;
+                    using (var stream = new MemoryStream())
+                    {
+                        request.InputStream.CopyTo(stream);
+                        file = stream.ToArray();
+                    }
+                    File.WriteAllBytes(StorageFolder + (request.RawUrl).Replace("/", "\\") + "\\" + request.Headers["FileName"], file);
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch
+            {
+
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+        static void PerformPOST(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                if (Directory.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+                {
+
+                    byte[] file;
+                    using (var stream = new MemoryStream())
+                    {
+                        request.InputStream.CopyTo(stream);
+                        file = stream.ToArray();
+                    }
+                    using (var stream = new FileStream(StorageFolder + (request.RawUrl).Replace("/", "\\") + "\\" + request.Headers["FileName"], FileMode.Append))
+                    {
+                        stream.Write(file, 0, file.Length);
+                    }
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch 
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+        static void PerformDELETE(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                if (File.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+                {
+                    File.Delete(StorageFolder + (request.RawUrl).Replace("/", "\\"));
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch 
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+        static void PerformCOPY(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                if ((File.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+        & (Directory.Exists(StorageFolder + "\\" + (request.Headers["Pathto"]).Replace("/", "\\"))))
+                {
+                    File.Copy(StorageFolder + (request.RawUrl).Replace("/", "\\"),
+                        StorageFolder + "\\" + (request.Headers["Pathto"]).Replace("/", "\\") + "\\"
+                        + Path.GetFileName(StorageFolder + (request.RawUrl).Replace("/", "\\"))
+                        );
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch 
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+        static void PerformMOVE(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            try
+            {
+                if ((File.Exists(StorageFolder + (request.RawUrl).Replace("/", "\\")))
+                    & (Directory.Exists(StorageFolder + "\\" + (request.Headers["Pathto"]).Replace("/", "\\"))))
+                {
+                    File.Move(StorageFolder + (request.RawUrl).Replace("/", "\\"),
+                        StorageFolder + "\\" + (request.Headers["Pathto"]).Replace("/", "\\") + "\\"
+                        + Path.GetFileName(StorageFolder + (request.RawUrl).Replace("/", "\\"))
+                        );
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.StatusDescription = "OK";
+                    response.OutputStream.Close();
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.StatusDescription = "Not found";
+                    response.OutputStream.Close();
+                }
+            }
+            catch 
+            {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                response.StatusDescription = "MethodNotAllowed";
+                response.OutputStream.Close();
+            }
+        }
+
+    }
+}
